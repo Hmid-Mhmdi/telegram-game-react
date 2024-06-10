dedf1616fef8c3874379bc28aa935d5d89cb46a1
Hamid Mohamadi <hamidpk0098@gmail.com>
2024-06-11 01:16:09 +0330

    add ton connect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import WebApp from '@twa-dev/sdk'
+import { TonConnectButton  } from '@tonconnect/ui-react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -35,6 +36,8 @@ function App() {
             Show Alert
         </button>
       </div>
+
+      <TonConnectButton />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import WebApp from '@twa-dev/sdk'
 import App from './App.tsx'
 import './index.css'
@@ -8,6 +9,8 @@ WebApp.ready()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl="https://hmid-mhmdi.github.io/telegram-game-react/tonconnect-manifest.json">
+       <App />
+    </TonConnectUIProvider>
   </React.StrictMode>,
 )
