52e58503f2e3d797fd4e1fc506cbc3eeafe027c5
Hamid Mohamadi <hamidpk0098@gmail.com>
2024-06-11 00:58:58 +0330

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-    build: {
-      outDir: './docs'
-    },
+  build: {
+      outDir: './dist'
+  },
   base: './',
-  root:"./docs"
 });
