59c9fcab0b0bd23d52c52a03eda137283d4feda9
Hamid Mohamadi <hamidpk0098@gmail.com>
2024-06-11 00:12:35 +0330

    test for fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import WebApp from '@twa-dev/sdk'
 import App from './App.tsx'
 import './index.css'
 
-// WebApp.ready();
+WebApp.ready()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
