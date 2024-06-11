329f3f082ca8946fa20509001cf92078e1f7e263
Hamid Mohamadi <hamidpk0098@gmail.com>
2024-06-11 20:41:39 +0330

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     errorElement: <NotFoundPage />,
     children: [
       {
-        path: "/",
+        path: "",
         element: <HomePage />,
       },
       {
