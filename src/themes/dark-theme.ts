5c3c06c7963cfd1a7eb0d3f4804ca0957339a8c5
Hamid Mohamadi <hamidpk0098@gmail.com>
2024-06-11 20:27:47 +0330

    fix issue

--- src/themes/dark-theme.ts
+++ src/themes/dark-theme.ts
@@ -1,7 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
-// A custom darkTheme for this app
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
